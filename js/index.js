9f18fb29805700f11362a5568fb244f0c4a31f56
alectramell <alectramell@gmail.com>
2017-10-16 23:29:44 -0700

    miniverse

---
+++ js/index.js
@@ -0,0 +1,49 @@
+function openHome() {
+	window.open('miniverse.html','MiniVerse','nonresizable,height=530,width=720');
+	return false;
+}
+
+function car_up() {
+	document.getElementById("car").src="img/cars/black_up.png";
+}
+
+function car_down() {
+	document.getElementById("car").src="img/cars/black_down.png";
+}
+
+function car_left() {
+	document.getElementById("car").src="img/cars/black_left.png";
+}
+
+function car_right() {
+	document.getElementById("car").src="img/cars/black_right.png";
+}
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+	document.getElementById("car").src="img/cars/black_up.png";
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+	document.getElementById("car").src="img/cars/black_down.png";
+    }
+    else if (e.keyCode == '37') {
+       // left arrow
+	document.getElementById("car").src="img/cars/black_left.png";
+    }
+    else if (e.keyCode == '39') {
+       // right arrow
+	document.getElementById("car").src="img/cars/black_right.png";
+    }
+    else if (e.keyCode == '187') {
+       // a
+	document.getElementById("car").src="img/cars/black_up_lights.png";
+    }
+
+}
